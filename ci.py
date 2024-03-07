e13f1a4ad99e43525f8ea5af5ddf6fbcfd84e6e6
Naveen RS <57190478+naveentnj@users.noreply.github.com>
2024-03-07 10:16:11 +0530

    Update ci.py

--- ci.py
+++ ci.py
@@ -1,3 +1,3 @@
 import wandb
 print(f'The version of wandb is: {wandb.__version__}')
-assert wandb.__version__ == '0.16.1', f'Expected version 0.16.1, but got {wandb.__version__}'
+assert wandb.__version__ == '0.16.0', f'Expected version 0.16.0, but got {wandb.__version__}'
